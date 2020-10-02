00428e643eec31fabbdfaf452a1e7de88c4ecf0f
Aryan Wolf <31042906+AryanWolf3D@users.noreply.github.com>
2020-10-02 23:00:08 +0100

    Added new file for misc. functions

---
+++ wl_utils.h
@@ -0,0 +1,14 @@
+// WL_UTILS.H
+
+#ifndef __WL_UTILS_H_
+#define __WL_UTILS_H_
+
+#include "wl_def.h"
+
+
+fixed    FixedMul (fixed a, fixed b);
+
+word     READWORD (byte *ptr);
+longword READLONGWORD (byte *ptr);
+
+#endif
