KS-Presto/Wolf4SDL

commit cd54cab8f4ee07e639a216bdf699182834abf0d1

Cleanup some map stuff
Replaced "mapon" usage with gamestate.mapon, made MAPPLANES 3 by default (because the maptype structure assumes 3 mapplanes minimum).

Also re-added the mapsize check.

id_ca.c
@@ -46,7 +46,7 @@ typedef struct
 typedef struct
 {
     word RLEWtag;
-    int32_t headeroffsets[100];
+    int32_t headeroffsets[NUMMAPS];
 } mapfiletype;
 
 
@@ -58,10 +58,8 @@ typedef struct
 =============================================================================
 */
 
-int     mapon;
-
 word    *mapsegs[MAPPLANES];
-static maptype* mapheaderseg[NUMMAPS];
+maptype *mapheaderseg[NUMMAPS];
 byte    *audiosegs[NUMSNDCHUNKS];
 byte    *grsegs[NUMCHUNKS];
 
@@ -596,7 +594,7 @@ void CAL_SetupMapFile (void)
 // allocate space for 3 64*64 planes
 //
     for (i=0;i<MAPPLANES;i++)
-        mapsegs[i] = SafeMalloc(maparea * sizeof(*mapsegs[i]));
+        mapsegs[i] = SafeMalloc(MAPAREA * sizeof(*mapsegs[i]));
 }
 
 
@@ -660,8 +658,6 @@ void CA_Startup (void)
     CAL_SetupMapFile ();
     CAL_SetupGrFile ();
     CAL_SetupAudioFile ();
-
-    mapon = -1;
 }
 
 //==========================================================================
@@ -1020,12 +1016,13 @@ void CA_CacheMap (int mapnum)
     int32_t   expanded;
 #endif
 
-    mapon = mapnum;
+    if (mapheaderseg[mapnum]->width != MAPSIZE || mapheaderseg[mapnum]->height != MAPSIZE)
+        Quit ("CA_CacheMap: Map not %u*%u!",MAPSIZE,MAPSIZE);
 
 //
 // load the planes into the allready allocated buffers
 //
-    size = maparea * sizeof(*dest);
+    size = MAPAREA * sizeof(*dest);
 
     for (plane = 0; plane<MAPPLANES; plane++)
     {

id_ca.h
@@ -4,11 +4,7 @@
 //===========================================================================
 
 #define NUMMAPS         60
-#ifdef USE_FLOORCEILINGTEX
-    #define MAPPLANES       3
-#else
-    #define MAPPLANES       2
-#endif
+#define MAPPLANES       3
 
 #define UNCACHEAUDIOCHUNK(chunk) {if(audiosegs[chunk]) {free(audiosegs[chunk]); audiosegs[chunk]=NULL;}}
 
@@ -16,19 +12,18 @@
 
 typedef struct
 {
-    int32_t planestart[3];
-    word    planelength[3];
+    int32_t planestart[MAPPLANES];
+    word    planelength[MAPPLANES];
     word    width,height;
     char    name[16];
 } maptype;
 
 //===========================================================================
 
-extern  int   mapon;
-
-extern  word *mapsegs[MAPPLANES];
-extern  byte *audiosegs[NUMSNDCHUNKS];
-extern  byte *grsegs[NUMCHUNKS];
+extern  word    *mapsegs[MAPPLANES];
+extern  maptype *mapheaderseg[NUMMAPS];
+extern  byte    *audiosegs[NUMSNDCHUNKS];
+extern  byte    *grsegs[NUMCHUNKS];
 
 extern  char  extension[5];
 extern  char  graphext[5];

wl_act2.c
@@ -1068,9 +1068,9 @@ void A_DeathScream (objtype *ob)
 {
 #ifndef UPLOAD
 #ifndef SPEAR
-    if (mapon==9 && !US_RndT())
+    if (gamestate.mapon==9 && !US_RndT())
 #else
-    if ((mapon==18 || mapon==19) && !US_RndT())
+    if ((gamestate.mapon==18 || gamestate.mapon==19) && !US_RndT())
 #endif
     {
         switch(ob->obclass)

wl_cloudsky.c
@@ -75,7 +75,7 @@ static int GetCloudSkyDefID()
 static int GetCloudSkyDefID()
 {
     int skyID;
-    switch(gamestate.episode * 10 + mapon)
+    switch(gamestate.episode * 10 + gamestate.mapon)
     {
         case  0: skyID =  0; break;
         case  1: skyID =  1; break;

wl_def.h
@@ -209,11 +209,8 @@ typedef uint8_t tiletype;
 #define MINDIST         (0x5800l)
 
 #define mapshift        6
-#define MAPSIZE         (1<<mapshift)
-#define maparea         MAPSIZE*MAPSIZE
-
-#define mapheight       MAPSIZE
-#define mapwidth        MAPSIZE
+#define MAPSIZE         (1 << mapshift)
+#define MAPAREA         (MAPSIZE * MAPSIZE)
 
 #ifdef USE_HIRES
 
@@ -1034,6 +1031,7 @@ extern  objtype         *actorat[MAPSIZE][MAPSIZE];
 
 extern  objtype         *player;
 
+extern  word            mapwidth,mapheight;
 extern  unsigned        tics;
 extern  int             viewsize;
 

wl_draw.c
@@ -696,7 +696,7 @@ byte vgaCeiling[]=
 
 void VGAClearScreen (void)
 {
-    byte ceiling=vgaCeiling[gamestate.episode*10+mapon];
+    byte ceiling=vgaCeiling[gamestate.episode*10+gamestate.mapon];
 
     int y;
     byte *dest = vbuf;
@@ -1603,7 +1603,7 @@ void ThreeDRefresh (void)
 //
 // clear out the traced array
 //
-    memset(spotvis,0,maparea);
+    memset(spotvis,0,MAPAREA);
 #ifdef PLAYDEMOLIKEORIGINAL      // ADDEDFIX 30 - Chris
     if (DEMOCOND_SDL)
 #endif

wl_game.c
@@ -632,6 +632,7 @@ static void ScanInfoPlane(void)
 void SetupGameLevel (void)
 {
     int  x,y;
+    int  mapnum;
     word *map;
     word tile;
 
@@ -658,8 +659,12 @@ void SetupGameLevel (void)
 //
 // load the level
 //
-    CA_CacheMap (gamestate.mapon+10*gamestate.episode);
-    mapon-=gamestate.episode*10;
+    mapnum = gamestate.mapon+10*gamestate.episode;
+
+    CA_CacheMap (mapnum);
+
+    mapwidth = mapheaderseg[mapnum]->width;
+    mapheight = mapheaderseg[mapnum]->height;
 
 #ifdef USE_FEATUREFLAGS
     // Temporary definition to make things clearer

wl_inter.c
@@ -550,9 +550,9 @@ LevelCompleted (void)
     VWB_DrawPic (0, 16, L_GUYPIC);
 
 #ifndef SPEAR
-    if (mapon < 8)
+    if (gamestate.mapon < 8)
 #else
-    if (mapon != 4 && mapon != 9 && mapon != 15 && mapon < 17)
+    if (gamestate.mapon != 4 && gamestate.mapon != 9 && gamestate.mapon != 15 && gamestate.mapon < 17)
 #endif
     {
 #ifndef JAPAN
@@ -580,9 +580,9 @@ LevelCompleted (void)
 #endif
 
 #ifdef SPANISH
-        Write (30, 12, parTimes[gamestate.episode * 10 + mapon].timestr);
+        Write (30, 12, parTimes[gamestate.episode * 10 + gamestate.mapon].timestr);
 #else
-        Write (26, 12, parTimes[gamestate.episode * 10 + mapon].timestr);
+        Write (26, 12, parTimes[gamestate.episode * 10 + gamestate.mapon].timestr);
 #endif
 
         //
@@ -593,8 +593,8 @@ LevelCompleted (void)
         if (sec > 99 * 60)      // 99 minutes max
             sec = 99 * 60;
 
-        if (gamestate.TimeCount < parTimes[gamestate.episode * 10 + mapon].time * 4200)
-            timeleft = (int32_t) ((parTimes[gamestate.episode * 10 + mapon].time * 4200) / 70 - sec);
+        if (gamestate.TimeCount < parTimes[gamestate.episode * 10 + gamestate.mapon].time * 4200)
+            timeleft = (int32_t) ((parTimes[gamestate.episode * 10 + gamestate.mapon].time * 4200) / 70 - sec);
 
         min = sec / 60;
         sec %= 60;
@@ -814,16 +814,16 @@ done:   itoa (kr, tempstr, 10);
         //
         // SAVE RATIO INFORMATION FOR ENDGAME
         //
-        LevelRatios[mapon].kill = kr;
-        LevelRatios[mapon].secret = sr;
-        LevelRatios[mapon].treasure = tr;
-        LevelRatios[mapon].time = min * 60 + sec;
+        LevelRatios[gamestate.mapon].kill = kr;
+        LevelRatios[gamestate.mapon].secret = sr;
+        LevelRatios[gamestate.mapon].treasure = tr;
+        LevelRatios[gamestate.mapon].time = min * 60 + sec;
     }
     else
     {
 #ifdef SPEAR
 #ifndef SPEARDEMO
-        switch (mapon)
+        switch (gamestate.mapon)
         {
             case 4:
                 Write (14, 4, " trans\n" " grosse\n" STR_DEFEATED);

commit 32a7cd7cf1be28918fa982dc9eec8c8152507dbf

Don't try to load an empty map plane
cd54cab introduced this bug; it probably didn't cause crashes because CAL_CarmackExpand does nothing with a zero length, however it will almost certainly crash when loading maps that don't use Carmack compression, because RLEWexpand has no safeguard against a zero length, since it assumes an expanded length of 4096 words per map plane. This will cause the source pointer to overflow.

id_ca.c
@@ -1022,6 +1022,9 @@ void CA_CacheMap (int mapnum)
         pos = mapheaderseg[mapnum]->planestart[plane];
         compressed = mapheaderseg[mapnum]->planelength[plane];
 
+	if (!compressed)
+	    continue;    // empty plane
+
         dest = mapsegs[plane];
 
         fseek (file,pos,SEEK_SET);